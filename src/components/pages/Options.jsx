e0dbc800f8820ace2c79d296390dffdf151a5ec4
maestrocodes06 <188086004+maestrocodes06@users.noreply.github.com.>
2025-08-20 15:34:16 +0530

    updated

---
+++ src/components/pages/Options.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Options = () => {
+  return (
+    <div>
+      <h1>Here I will Tll You About the things That you Hate My Baby </h1>
+    </div>
+  )
+}
+
+export default Options
